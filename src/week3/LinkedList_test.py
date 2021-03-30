0537354cf1c479cab2d3995f8e5c39c498652608
GeunChang Ahn <rkaehdaos@gmail.com>
2021-03-30 21:15:56 +0900

    주피터 노트북 추가 - 링크드 리스트 수정 및 연습

---
+++ src/week3/LinkedList_test.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class NodeMgmt:  # Node 관리
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def add(self, data):
+        if self.head == '':
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
