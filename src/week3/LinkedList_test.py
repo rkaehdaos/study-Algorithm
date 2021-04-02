b6fb929c379920a446801f2032c8cb36b992753b
GeunChang Ahn <rkaehdaos@gmail.com>
2021-04-03 00:11:53 +0900

    링크드 리스트 삭제

--- src/week3/LinkedList_test.py
+++ src/week3/LinkedList_test.py
@@ -23,9 +23,37 @@ class NodeMgmt:  # Node 관리
             print(node.data)
             node = node.next
 
+    ## 삭제 함수 만들어보기
+    ## 1. head
+    ## 2. 중간
+    ## 3. 끝
+    def delete(self, data):
+        if self.head=='':
+            print('node가 없음')
+            return
+
+        if self.head.data==data:
+            temp=self.head
+            self.head=self.head.next
+            del temp ## 객체 삭제
+        else:
+            node=self.head
+            while node.next:
+                if node.next.data==data:
+                    temp=node.next
+                    node.next=node.next.next
+                    del temp
+                else:
+                    node=node.next
+        return
+
+
 # Test
 linkedlist1 = NodeMgmt(0)
 linkedlist1.desc()
 for data in range(1, 10):
-    linkedlist1.add(data)
+    linkedlist1.add(data)  # 맨끝에 추가
+linkedlist1.desc()  # 데이터 순회하며 출력
+print('--------------------------------------------')
+linkedlist1.delete(5)
 linkedlist1.desc()
