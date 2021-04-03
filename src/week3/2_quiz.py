fd4e332ce0a75a8c6a228fc26642459ee9ba3964
GeunChang Ahn <rkaehdaos@gmail.com>
2021-04-03 17:19:54 +0900

    3 주차 퀴즈 제출

---
+++ src/week3/2_quiz.py
@@ -0,0 +1,18 @@
+# q3
+def hash_func(n):
+    return abs((n % 10) - 5)
+
+
+for i in range(1, 100):
+    print(hash_func(i))
+# 0~5 : 6개
+
+# q4
+import queue
+
+a = queue.PriorityQueue()
+a.put((10, '캠퍼스'))
+a.put((1, '패스트'))
+a.put((55, '완주반'))
+a.put((11, '온라인'))
+print(a.get()[1], a.get()[1], a.get()[1], a.get()[1])
