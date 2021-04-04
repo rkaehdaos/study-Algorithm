0acb511ef88a0f6162c12090e18412e7951d6954
GeunChang Ahn <rkaehdaos@gmail.com>
2021-04-04 12:00:35 +0900

    3 주차 과제4 - 제출 구현

--- src/week3/3_homework_q4.py
+++ src/week3/3_homework_q4.py
@@ -33,8 +33,28 @@ class Node:
 
 
 class ChainedHashTable(HashTable):
-    pass
+    def set(self, key, value):
+        if self.table[hash_func(key)] != None:
+            node = self.table[hash_func(key)]
+            while node.next:
+                if node.key == key:
+                    node.data = value
+                    return
+                node = node.next
+            node.next = Node(key, value)
+        else:
+            self.table[hash_func(key)] = Node(key, value)
 
+    def get(self, key):
+        if self.table[hash_func(key)] != None:
+            node = self.table[hash_func(key)]
+            while node:
+                if node.key == key:
+                    return node.data
+                node = node.next
+            return None
+        else:
+            return None
 
 # Test code
 
