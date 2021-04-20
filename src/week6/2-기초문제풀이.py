723be48d62bd5770b61424fbe88bcd56752f33d8
GeunChang Ahn <rkaehdaos@gmail.com>
2021-04-21 00:04:46 +0900

    week6 - 기초2 내 풀이 - 문제는 예제는 통과했는데 백준에 제출하면 돼지 않음..

--- src/week6/2-기초문제풀이.py
+++ src/week6/2-기초문제풀이.py
@@ -4,12 +4,26 @@
 import sys
 
 a = list(map(int, sys.stdin.readline().split(' ')))
-b = list(map(int, sys.stdin.readline().split(' ')))
+cards = list(map(int, sys.stdin.readline().split(' ')))
+
 N = a[0]
 M = a[1]
 result = 0
-print(N, M)
-b.sort()
-print(b)
-# 조합
-
+cards.sort(reverse=True)
+result = 0
+count = 0
+sum = 0
+for i in range(N):
+    sum = cards[i]
+    count = 1
+    for j in range(N):
+        if i == j or sum + cards[j] > M:
+            continue
+        else:
+            sum += cards[j]
+            count += 1
+        if count == 3:
+            break
+    if count == 3 and abs(M - result) > abs(M - sum):
+        result = sum
+print(result)
